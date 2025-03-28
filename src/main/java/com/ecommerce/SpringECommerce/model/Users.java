49b6e43e1c3049dbed4f9f00a19546dda99f5f6a
Toan <anhtoan@posco.net>
2025-03-28 15:47:24 +0700

    Update Spring Security setup config JWT

--- src/main/java/com/ecommerce/SpringECommerce/model/Users.java
+++ src/main/java/com/ecommerce/SpringECommerce/model/Users.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
 public class Users {
 
     @Id
@@ -14,4 +13,36 @@ public class Users {
     private String username;
     private String password;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
